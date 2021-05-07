627f1c57d398ec1b60a7628d2f3fa62b5f9aae67
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-05-07 08:04:26 +0800

    added modal for form submit

---
+++ dollosa-michael/landing-page/js/index.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector(".success-modal")
+const submit = document.querySelector("#form")
+const modalBtn = document.querySelector("#modal-btn")
+
+submit.addEventListener('submit', e => {
+  modal.removeAttribute("id")
+  console.log("event function")
+  console.log(modal)
+  e.preventDefault();
+  
+});
+
+modalBtn.addEventListener('click', e => {
+  modal.id = "modal"
+
+});
