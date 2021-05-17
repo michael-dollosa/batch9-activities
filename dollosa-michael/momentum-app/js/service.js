7d05ef45d14f655e2b3020df75335f31c6980f7d
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-05-18 04:23:52 +0800

    v1.0.0 of momentum app

--- dollosa-michael/momentum-app/js/service.js
+++ dollosa-michael/momentum-app/js/service.js
@@ -4,13 +4,9 @@ export const getTime = () => {
   return (dateTime.getHours()<10?'0':'') + dateTime.getHours() + ":" + (dateTime.getMinutes()<10?'0':'') + dateTime.getMinutes()
 }
 
-//get quote
-
+//fetch quote API
 export const getQuote = async() => {
   const res = await fetch("https://complimentr.com/api")
   const data = await res.json()
   return data
-  //edit quote line with random quote
-  // quote.innerHTML = data.compliment
-  // console.log(data)
 }
