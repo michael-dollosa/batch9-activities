8df890d26dd815e538342adf94eae44f29da0c04
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-05-31 03:29:25 +0800

    modified border styling to be dynamic based on rows and cols set on variable

--- dollosa-michael/tictactoe/js/creation.js
+++ dollosa-michael/tictactoe/js/creation.js
@@ -2,6 +2,10 @@ const createGrid = (rows, cols) => {
   let gridContainer = document.querySelector("#tictactoe-container")
   let grid = rows*cols
 
+  //set grid based on number of rows and cols
+  gridContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
+  gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+
   //creation of frontend grid
   for(let x = 0; x < rows; x++) {
     for(let y = 0; y < cols; y++) {
@@ -9,6 +13,23 @@ const createGrid = (rows, cols) => {
       item.setAttribute("row", x)
       item.setAttribute("col", y)
       gridContainer.append(item)
+
+      //setting oustide borders to none
+      if(x === 0){
+        item.style.borderTop = "none"
+      }
+
+      if(x === rows-1) {
+        item.style.borderBottom = "none"
+      }
+
+      if(y === 0) {
+        item.style.borderLeft = "none"
+      }
+
+      if(y === cols-1){
+        item.style.borderRight = "none"
+      }
     }
   }
 }
